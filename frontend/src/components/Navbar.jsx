ba06eca226e08ceb1af39cb015fe3f7ef5ff4a04
arko1995 <shahriarsohelarko@gmail.com>
2026-05-17 11:59:02 +0600

    started frontend

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { ShoppingCart, UserPlus, LogIn, LogOut, Lock } from "lucide-react";
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
