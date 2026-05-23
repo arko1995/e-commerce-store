aaceb92d71747a0764f22ca062c5337dc0208837
arko1995 <shahriarsohelarko@gmail.com>
2026-05-23 19:07:22 +0600

    working on image upload

--- frontend/src/components/CreateProductForm.jsx
+++ frontend/src/components/CreateProductForm.jsx
@@ -29,6 +29,22 @@ const CreateProductForm = () => {
     console.log(newProduct);
   };
 
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      console.log(reader);
+
+      reader.onloadend = () => {
+        console.log(reader.result);
+
+        setNewProduct({ ...newProduct, image: reader.result });
+      };
+
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
     <motion.div
       className="bg-gray-800 shadow-lg rounded-lg p-8 mb-8 max-w-xl mx-auto"
@@ -140,7 +156,13 @@ const CreateProductForm = () => {
         </div>
 
         <div className="mt-1 flex items-center">
-          <input type="file" id="image" className="sr-only" accept="image/*" />
+          <input
+            type="file"
+            id="image"
+            className="sr-only"
+            accept="image/*"
+            onChange={handleImageChange}
+          />
           <label
             htmlFor="image"
             className="cursor-pointer bg-gray-700 py-2 px-3 border border-gray-600 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-300 hover:bg-gray-600 focus:outline-none focus:ring-2 
@@ -150,9 +172,7 @@ const CreateProductForm = () => {
             Upload Image
           </label>
           {newProduct.image && (
-            <span className="ml-3 text-sm text-gray-400">
-              {newProduct.image}
-            </span>
+            <span className="ml-3 text-sm text-gray-400">Image Uploaded</span>
           )}
         </div>
 
