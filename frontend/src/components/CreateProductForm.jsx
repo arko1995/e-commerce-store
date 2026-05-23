8c45b964448864b2e1d8a76c09bc11e4324fa3d2
arko1995 <shahriarsohelarko@gmail.com>
2026-05-23 18:07:07 +0600

    creating product form

--- frontend/src/components/CreateProductForm.jsx
+++ frontend/src/components/CreateProductForm.jsx
@@ -22,8 +22,11 @@ const CreateProductForm = () => {
     image: "",
   });
 
-  const handleSubmit = () => {
-    console.log(FormData);
+  const loading = false;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(newProduct);
   };
 
   return (
@@ -37,7 +40,141 @@ const CreateProductForm = () => {
         Create New Product
       </h2>
 
-      <form onSubmit={handleSubmit}></form>
+      <form onSubmit={handleSubmit} className="space-y-4">
+        <div>
+          <label
+            htmlFor="name"
+            className="block text-sm font-medium text-gray-300 text-left"
+          >
+            Product Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={newProduct.name}
+            onChange={(e) =>
+              setNewProduct(() => ({
+                ...newProduct,
+                name: e.target.value,
+              }))
+            }
+            className="mt-1 block w-full bg-gray-700 border border-gray-600 rounded-md shadow-sm py-2 px-3 text-white focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:border-emerald-500"
+            required
+          />
+        </div>
+
+        <div>
+          <label
+            htmlFor="description"
+            className="block text-sm font-medium text-gray-300 text-left"
+          >
+            Description
+          </label>
+          <textarea
+            type="text"
+            id="description"
+            name="description"
+            value={newProduct.description}
+            onChange={(e) =>
+              setNewProduct(() => ({
+                ...newProduct,
+                description: e.target.value,
+              }))
+            }
+            rows="3"
+            className="mt-1 block w-full bg-gray-700 border border-gray-600 rounded-md shadow-sm py-2 px-3 text-white focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:border-emerald-500"
+            required
+          />
+        </div>
+
+        <div>
+          <label
+            htmlFor="price"
+            className="block text-sm font-medium text-gray-300 text-left"
+          >
+            Price
+          </label>
+          <input
+            type="number"
+            id="price"
+            name="price"
+            value={newProduct.price}
+            onChange={(e) =>
+              setNewProduct(() => ({
+                ...newProduct,
+                price: e.target.value,
+              }))
+            }
+            step="0.01"
+            className="mt-1 block w-full bg-gray-700 border border-gray-600 rounded-md shadow-sm py-2 px-3 text-white focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:border-emerald-500"
+            required
+          />
+        </div>
+
+        <div>
+          <label
+            htmlFor="category"
+            className="block text-sm font-medium text-gray-300 text-left"
+          >
+            Category
+          </label>
+          <select
+            name="category"
+            id="category"
+            value={newProduct.category}
+            onChange={(e) =>
+              setNewProduct(() => ({ ...newProduct, category: e.target.value }))
+            }
+            className="mt-1 block w-full bg-gray-700 border border-gray-600 rounded-md shadow-sm py-2 px-3 text-white focus:outline-none
+            focus:ring-2 focus:ring-emerald-500 focus:border-emerald-500"
+            required
+          >
+            <option value="">Select a Category</option>
+            {categories.map((category) => (
+              <option value="category" key="category">
+                {category}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="mt-1 flex items-center">
+          <input type="file" id="image" className="sr-only" accept="image/*" />
+          <label
+            htmlFor="image"
+            className="cursor-pointer bg-gray-700 py-2 px-3 border border-gray-600 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-300 hover:bg-gray-600 focus:outline-none focus:ring-2 
+          focus:ring-offset-2 focus:ring-emerald-500"
+          >
+            <Upload className="h-5 w-5 inline-block mr-2" />
+            Upload Image
+          </label>
+          {newProduct.image && (
+            <span className="ml-3 text-sm text-gray-400">
+              {newProduct.image}
+            </span>
+          )}
+        </div>
+
+        <button
+          type="submit"
+          className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-emerald-600 hover:bg-emerald-700 
+        focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-600 disabled:opacity-50"
+          disabled={loading}
+        >
+          {loading ? (
+            <>
+              <Loader className="mr-2 h-5 w-5 animate-spin" />
+              loading....
+            </>
+          ) : (
+            <>
+              <PlusCircle className="mr-2 h-5 w-5" />
+              Create a Product
+            </>
+          )}
+        </button>
+      </form>
     </motion.div>
   );
 };
