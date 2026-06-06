1161baba810ebaedef2ecac17adedfc63c8bd6de
arko1995 <shahriarsohelarko@gmail.com>
2026-06-06 14:31:54 +0600

    analytics chart

--- frontend/src/components/AnalyticsTab.jsx
+++ frontend/src/components/AnalyticsTab.jsx
@@ -2,6 +2,16 @@ import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import axiosInstance from "../lib/axios";
 import { Users, Package, ShoppingCart, DollarSign } from "lucide-react";
+import {
+  ResponsiveContainer,
+  LineChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  Line,
+} from "recharts";
 
 const AnalyticsTab = () => {
   const [analyticsData, setAnalyticsData] = useState({
@@ -19,6 +29,7 @@ const AnalyticsTab = () => {
     const fetchAnalyticsData = async () => {
       try {
         const response = await axiosInstance.get("/analytics");
+
         setAnalyticsData(response.data.data.analyticsData);
         setDailySalesData(response.data.data.dailySalesData);
       } catch (error) {
@@ -60,6 +71,39 @@ const AnalyticsTab = () => {
           color="from-emerald-500 to-lime-700"
         />
       </div>
+      <motion.div
+        className="bg-gray-800/60 rounded-lg p-6 shadow-lg"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.25 }}
+      >
+        <ResponsiveContainer width="100%" height={400}>
+          <LineChart data={dailySalesData}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" stroke="#D1D5DB" />
+            <YAxis yAxisId="left" stroke="#D1D5DB" />
+            <YAxis yAxisId="right" orientation="right" stroke="#D1D5DB" />
+            <Tooltip />
+            <Legend />
+            <Line
+              yAxisId="left"
+              type="monotone"
+              dataKey="sales"
+              stroke="#10B981"
+              activeDot={{ r: 8 }}
+              name="Sales"
+            />
+            <Line
+              yAxisId="right"
+              type="monotone"
+              dataKey="revenue"
+              stroke="#3B82F6"
+              activeDot={{ r: 8 }}
+              name="Revenue"
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </motion.div>
     </div>
   );
 };
