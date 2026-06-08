04b90021b75e18c64f74663bdc64438bcda467f1
arko1995 <shahriarsohelarko@gmail.com>
2026-06-08 16:34:28 +0600

    fiished project

--- frontend/src/stores/useUserStore.js
+++ frontend/src/stores/useUserStore.js
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import axiosInstance from "../lib/axios.js";
 import toast from "react-hot-toast";
+import { refreshToken } from "../../../backend/controller/auth.controller.js";
 
 export const useUserStore = create((set, get) => ({
   user: null,
@@ -66,6 +67,19 @@ export const useUserStore = create((set, get) => ({
       toast.error(error.response?.data?.message || "An error occurred");
     }
   },
+
+  refreshToken: async () => {
+    if (get().checkingAuth) return;
+    set({ checkingAuth: true });
+    try {
+      const response = await axiosInstance.post("/auth/refresh-token");
+      set({ checkingAuth: false });
+      return response.data.data;
+    } catch (error) {
+      set({ user: null, checkingAuth: false });
+      throw error;
+    }
+  },
 }));
 
 let refreshPromise = null;
@@ -75,8 +89,23 @@ axiosInstance.interceptors.response.use(
   async (error) => {
     const originalRequest = error.config; //error.config contains the configuration of a failed http request
 
-    if(error.response?.status === 401){
-      
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        if (refreshPromise) {
+          await refreshPromise;
+          return axiosInstance(originalRequest);
+        }
+        refreshPromise = useUserStore.getState().refreshToken();
+        await refreshPromise;
+        refreshPromise = null;
+
+        return axiosInstance(originalRequest);
+      } catch (refreshError) {
+        useUserStore.getState().logOut();
+        return Promise.reject(refreshError);
+      }
     }
+    return Promise.reject(error);
   },
 );
